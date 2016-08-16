d22a120281047254d2938bb2b05ad1473bbd4315
Frontier - <Frontier ->
2016-08-16 13:37:27 +0300

    added header

---
+++ src/components/headTop.vue
@@ -0,0 +1,55 @@
+<template>
+  <header class="head">
+    <div class="head__inner">
+      <div class="head__buttons-wrap">
+        <button class="btn">Добавить</button>
+        <button class="btn">Обновить</button>
+      </div>
+      <div class="head__search-wrap">
+        <slot></slot>
+      </div>
+    </div>
+  </header>
+</template>
+
+<style>
+  .head {
+    background: #DADFE1;
+    padding: 20px;
+  }
+
+  .head__inner {
+    max-width: 960px;
+    margin: auto;
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .btn {
+    font-size: 1em;
+    background: #4183D7;
+    color: #fff;
+    border: 0;
+    outline: 0;
+    padding: 6px 12px;
+    border-radius: 3px;
+    cursor: pointer;
+    transition: background-color .3s;
+  }
+
+  .btn:hover {
+    background: #59ABE3;
+  }
+
+  .btn:active {
+    background: #446CB3;
+  }
+
+  .head__buttons-wrap .btn {
+    margin-right: 10px;
+  }
+</style>
+
+<script>
+export default {};
+</script>
---
+++ src/components/search.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="search">
+    <form action="#">
+      <button class="btn">Go</button><input type="text" class="search__input" placeholder="Событие, дата или участник">
+    </form>
+  </div>
+</template>
+
+<style>
+.search__input {
+  padding: 6px 12px;
+  border: 1px solid #4183D7;
+  border-radius: 0 3px 3px 0;
+  outline: 0;
+  transition: border-color .3s;
+  width: 204px;
+}
+
+.search .btn {
+  border-radius: 3px 0 0 3px;
+}
+
+.search .btn:hover + .search__input {
+  border-color: #59ABE3;
+}
+
+.search .btn:active + .search__input {
+  border-color: #446CB3;
+}
+
+.search form {
+  display: flex;
+}
+</style>
+
+<script>
+export default {};
+</script>
